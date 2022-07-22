e311627168b692d5ac9fc80d9286d5a54a0a9bda
juandrt5416 <juandrt5416@gmail.com>
2022-07-21 22:01:10 -0500

    Correcciones ejs

--- src/app.js
+++ src/app.js
@@ -1,20 +1,17 @@
 const express = require('express');
-const path = require('path');
+const path = require('path')
 
 const app = express();
 
-var indexRouter = require('./routes/main');
+var indexMain = require('./routes/main');
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/static', express.static(path.join(__dirname, '../public')))
 
-app.use('/', indexRouter);
+app.use('/', indexMain);
 
 app.listen(3000, () => {
   console.log("Servidor grupo_6_OK");
 });
-
-module.exports = app;
