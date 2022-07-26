8e4a5afb1a38d2a74251997c096fa86a36d8c591
juandrt5416 <juandrt5416@gmail.com>
2022-07-25 19:05:33 -0500

    Cambios en las paginas productEdit y productLoad

--- src/app.js
+++ src/app.js
@@ -1,14 +1,17 @@
 const express = require('express');
+const favicon = require('serve-favicon');
 const path = require('path')
 
 const app = express();
 
-var indexMain = require('./routes/main');
+const indexMain = require('./routes/main');
 
 // view engine setup
 app.set('view engine', 'ejs');
 
-app.use('/static', express.static(path.join(__dirname, '../public')))
+app.use('/static', express.static(path.join(__dirname, '../public')));
+
+app.use(favicon(__dirname + '/favicon.ico'));
 
 app.use('/', indexMain);
 
