e47e24ce478ef88cda74bbee8bc414f0ee41f884
juandrt5416 <juandrt5416@gmail.com>
2022-08-01 21:00:17 -0500

    Configuraciones de CRUD

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const favicon = require('serve-favicon');
-const path = require('path')
+const path = require('path');
+const methodOverride = require('method-override');
 
 const app = express();
 
@@ -18,6 +19,7 @@ app.use(favicon(__dirname + '/favicon.ico'));
 
 app.use(express.urlencoded({ extended: false}));
 app.use(express.json());
+app.use(methodOverride("_method"))
 
 app.use('/', routerMain);
 app.use('/products', routerProducts);
--- src/routes/products.js
+++ src/routes/products.js
@@ -1,6 +1,18 @@
 var express = require('express');
 var router = express.Router();
+const path = require('path');
+const multer = require('multer');
 const productController = require('../controllers/productsController');
+
+const storage = multer.diskStorage({ 
+    destination: function (req, file, cb) { 
+       cb(null, './public/images/img'); 
+    }, 
+    filename: function (req, file, cb) { 
+       cb(null, `${Date.now()}_img_${path.extname(file.originalname)}`);  } 
+  });
+
+const uploadFile = multer({ storage });
 
 /* GET home page. */
 router.get('/productDetail/:comicId', productController.productDetail);
@@ -8,5 +20,6 @@ router.get('/productCart', productController.productCart);
 router.get('/productEdit', productController.productEdit);
 router.get('/productList', productController.productList);
 router.get('/productload', productController.productload);
+router.post('/productload', uploadFile.single('img'), productController.productCreated);
 
 module.exports = router;
