ba21a63694d6df587db546cfdf3e9817d6b7a7e4
juandrt5416 <juandrt5416@gmail.com>
2022-08-02 22:47:23 -0500

    Co-authored-by: jsebas980 <jsebas980@users.noreply.github.com> Co-authored-by: Andres Felipe Moreno C <A-moreno98@users.noreply.github.com>

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -3,21 +3,101 @@ let archivo = './database/products.json';
 let comicProductos = JSON.parse(fs.readFileSync(archivo, 'utf-8'))
 
 const productController = {
+    /* CONTROLLER productos */
+
+    /*** Obtener un producto ***/
     productDetail: (req, res) => {
         let producto = comicProductos.find(producto => producto.id == req.params.comicId);
         res.render("products/productDetail", { producto: producto });
     },
+    /*** Carrito de compras ***/
     productCart: (req, res) => {
         res.render("products/productCart");
     },
+    /*** Editar un producto OK***/
     productEdit: (req, res) => {
-        res.render("products/productEdit");
+		let id = req.params.id
+		let productToEdit = comicProductos.find(product => product.id == id)
+		res.render('products/productEdit', {productToEdit})
+	},
+
+    productUpdate: (req, res) => {
+        let id = req.params.id
+        let productToEdit = comicProductos.find(product => product.id == id)
+        let image
+
+        // if (req.files[0] != undefined) {
+        //     image = req.files[0].filename
+        // } else {
+        //     image = productToEdit.image
+        // }
+
+        productToEdit = {
+            id: Number(id),
+            titulo: req.body.titulo,
+            temporada: req.body.temporada,
+            volumen: req.body.volumen,
+            descripcionCorta: req.body.descripcionCorta,
+            descripcionDetallada: req.body.descripcionDetallada,
+            precioNormal: Number(req.body.precioNormal),
+            autor: req.body.autor,
+            illustrador: req.body.illustrador,
+            categoria: req.body.categoria,
+            publicacion: req.body.publicacion,
+            img: "01.jpg"
+        };
+
+        let newProducts = comicProductos.map(product => {
+            if (product.id == id) {
+                return product = { ...productToEdit };
+            }
+            return product;
+        })
+
+        fs.writeFileSync(archivo, JSON.stringify(newProducts, null, ' '));
+        res.render("products/productCart");
     },
+    /*** Eliminar un producto OK***/
+    productDelete: (req, res) => {
+        let id = req.params.id;
+        let finalProducts = comicProductos.filter(product => product.id != id);
+        fs.writeFileSync(archivo, JSON.stringify(finalProducts, null, ' '));
+        res.redirect('/');
+    },
+    /*** Imprimir todos los productos OK***/
     productList: (req, res) => {
-        res.render("products/productList");
+        res.render("products/productList", { comicProductos });
     },
+
+    /*** Crear un producto OK***/
+    productCreate: (req, res) => {
+		res.render('products/productLoad')
+	},
+
     productload: (req, res) => {
-        res.render("products/productload");
+        let image
+		if(req.files[0] != undefined){
+			image = req.files[0].filename
+		} else {
+			image = 'default.jpg'
+		}
+        let newProduct = {
+            id: comicProductos[comicProductos.length - 1].id + 1,
+            titulo: req.body.titulo,
+            temporada: req.body.temporada,
+            volumen: req.body.volumen,
+            descripcionCorta: req.body.descripcionCorta,
+            descripcionDetallada: req.body.descripcionDetallada,
+            precioNormal: req.body.precioNormal,
+            autor: req.body.autor,
+            illustrador: req.body.illustrador,
+            categoria: req.body.categoria,
+            publicacion: req.body.publicacion,
+            img: image
+        };
+        comicProductos.push(newProduct)
+        fs.writeFileSync(archivo, JSON.stringify(comicProductos, null, ' '));
+        res.redirect('index');
     },
 };
 
--- src/routes/products.js
+++ src/routes/products.js
@@ -1,25 +1,40 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 const path = require('path');
 const multer = require('multer');
+
 const productController = require('../controllers/productsController');
 
-const storage = multer.diskStorage({ 
-    destination: function (req, file, cb) { 
-       cb(null, './public/images/img'); 
-    }, 
-    filename: function (req, file, cb) { 
-       cb(null, `${Date.now()}_img_${path.extname(file.originalname)}`);  } 
-  });
+const storage = multer.diskStorage({
+   destination: function (req, file, cb) {
+      cb(null, '../public/images/img');
+   },
+   filename: function (req, file, cb) {
+      cb(null, file.fieldname + path.extname(file.originalname));
+   }
+});
+var uploadFile = multer({storage: storage})
 
-const uploadFile = multer({ storage });
+/* ROUTER productos */
 
-/* GET home page. */
+/*** Obtener un producto OK***/
 router.get('/productDetail/:comicId', productController.productDetail);
+
+/*** Carrito de compras ***/
 router.get('/productCart', productController.productCart);
-router.get('/productEdit', productController.productEdit);
+
+/*** Editar un producto OK***/
+router.get('/productEdit/:id', productController.productEdit);
+router.patch('/productEdit/:id', uploadFile.single('img'), productController.productUpdate);
+
+/*** Eliminar un producto OK***/
+router.delete('/productDelete/:id', productController.productDelete);
+
+/*** Imprimir todos los productos OK***/
 router.get('/productList', productController.productList);
-router.get('/productload', productController.productload);
-router.post('/productload', uploadFile.single('img'), productController.productCreated);
+
+/*** Crear un producto OK***/
+router.get('/productload', productController.productCreate);
+router.post('/', uploadFile.single('img'), productController.productload);
 
 module.exports = router;
